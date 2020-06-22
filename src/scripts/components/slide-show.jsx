a58600655f0d227267f9f6435cfd21f90d8029c6
rikutons <rikutonsicloud@gmail.com>
2020-06-22 18:46:47 +0900

    Improve slide-show appearance by to add bar and button

--- src/scripts/components/slide-show.jsx
+++ src/scripts/components/slide-show.jsx
@@ -17,7 +17,7 @@ function SlideShow() {
         setImageCount(nextCount)
     }
 
-    var intervalId = setInterval(changeImange, 2000)
+    var intervalId = setInterval(changeImange, 5000)
     useEffect(() => {
         return () => {
             clearInterval(intervalId)
@@ -25,20 +25,49 @@ function SlideShow() {
     })
 
     var imgTags = []
+    var buttons = []
     images.forEach((image, index) => {
-        var classNames = "slide-show__image"
+        var imageClassNames = "slide-show__image"
         if (index == 0)
-            classNames += " slide-show__image--first"
+            imageClassNames += " slide-show__image--first"
         imgTags.push(
             <CSSTransition in={imageCount == index} timeout={0} key={index} classNames="slide-show__image-">
-                <img className={classNames} src={imagePath + image}/>
+                <img className={imageClassNames} src={imagePath + image}/>
             </CSSTransition>
         )
+
+        var buttonClassNames = "slide-show__toggle-button"
+        if (index == 0)
+            buttonClassNames += " slide-show__toggle-button--first"
+        if (index != images.length - 1)
+            buttons.push(
+                <CSSTransition in={imageCount == index} timeout={0} key={index} classNames="slide-show__toggle-button-">
+                    <button className={buttonClassNames} onClick={() => setImageCount(index)} />
+                </CSSTransition>
+            )
+        else
+            buttons.push(
+                <CSSTransition in={imageCount == index} timeout={0} key={index}
+                    classNames={{
+                        enter: 'slide-show__toggle-button--enter',
+                        enterDone: 'slide-show__toggle-button--enter-done',
+                        exit: 'slide-show__toggle-button--exit',
+                        exitDone: 'slide-show__toggle-button-last--exit-done',
+                    }}
+                >
+                    <button className={buttonClassNames} onClick={() => setImageCount(index)} />
+                </CSSTransition>
+            )
     })
 
+    
     return (
         <div className="slide-show">
             {imgTags}
+            <div key={imageCount} className="slide-show__bar" />
+            <div className="slide-show__toggle-button-wrapper">
+                {buttons}
+            </div>
         </div>
     )
 }
