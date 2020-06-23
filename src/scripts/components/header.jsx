80c45bf13233b3cef0a997a452c3969c28c64ced
rikutons <rikutonsicloud@gmail.com>
2020-06-23 15:36:07 +0900

    Add ease scroll on haeder

--- src/scripts/components/header.jsx
+++ src/scripts/components/header.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { Link } from 'react-scroll'
 
 function Header() {
-    const scrollMillSecond = 500;
+    const scrollMillSecond = 10000;
     return (
         <div className="header">
-            <Link className="header__text header__text--blue" to="slide-show" smooth={true} duration={scrollMillSecond}>Sueta Portfolio</Link>
-            <Link className="header__text" to="about" smooth={true} duration={scrollMillSecond}>About</Link>
-            <Link className="header__text" to="works" smooth={true} duration={scrollMillSecond}>Works</Link>
-            <Link className="header__text" to="contact" smooth={true} duration={scrollMillSecond}>Contact</Link>
+            <Link className="header__text header__text--blue" to="slide-show" smooth='easeOutQuint' duration={scrollMillSecond}>Sueta Portfolio</Link>
+            <Link className="header__text" to="about" smooth='easeOutQuint' duration={scrollMillSecond}>About</Link>
+            <Link className="header__text" to="works" smooth='easeOutQuint' duration={scrollMillSecond}>Works</Link>
+            <Link className="header__text" to="contact" smooth='easeOutQuint' duration={scrollMillSecond}>Contact</Link>
         </div>
     )
 }
