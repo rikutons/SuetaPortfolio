6bfd6f160cbcd1bb25503592a6b1c6537258aebc
rikutons <rikutonsicloud@gmail.com>
2020-06-28 02:29:42 +0900

    Insert works link

--- src/scripts/components/contents/works.jsx
+++ src/scripts/components/contents/works.jsx
@@ -21,7 +21,7 @@ function Works() {
           Wander Tempo<br />
           Unityを利用した3D音楽ゲーム
         </Work>
-        <Work link="" imageID="1">
+        <Work link="https://github.com/rikutons/IntroOfMyCity" imageID="1">
           IntroOfMasuda<br />
           学校の課題製作<br />
           HTML + CSSを使った島根県益田市の紹介サイト
